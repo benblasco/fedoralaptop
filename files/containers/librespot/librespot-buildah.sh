363565dd746a30ea8fc50e707fe0b648083cde7d
Benjamin Blasco <eraser215@hotmail.com>
2022-02-25 11:52:36 +1100

    Added CONTAINER var instead of hard coding name

--- files/containers/librespot/librespot-buildah.sh
+++ files/containers/librespot/librespot-buildah.sh
@@ -3,17 +3,17 @@
 # Changes to be made: 
 # - Add labels for maintainer and description
 
-buildah from registry.fedoraproject.org/fedora-minimal:35
+CONTAINER=$(buildah from registry.fedoraproject.org/fedora-minimal:35)
 
-buildah run fedora-minimal-working-container microdnf install -y cargo alsa-lib-devel make gcc
-buildah run fedora-minimal-working-container microdnf clean all
-buildah run fedora-minimal-working-container rm -rf /var/cache/dnf
-buildah run fedora-minimal-working-container cargo install librespot
+buildah run ${CONTAINER} microdnf install -y cargo alsa-lib-devel make gcc
+buildah run ${CONTAINER} microdnf clean all
+buildah run ${CONTAINER} rm -rf /var/cache/dnf
+buildah run ${CONTAINER} cargo install librespot
 
-buildah config --entrypoint "/root/.cargo/bin/librespot" fedora-minimal-working-container 
-buildah config --cmd "--name=NUC LibreSpot Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" fedora-minimal-working-container
+buildah config --entrypoint "/root/.cargo/bin/librespot" ${CONTAINER} 
+buildah config --cmd "--name=NUC LibreSpot Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
 
 buildah config --label maintainer="Benjamin Blasco"
 buildah config --label description="Fedora-based container for running librespot, built on Rust"
 
-buildah commit fedora-minimal-working-container librespot-container
+buildah commit ${CONTAINER} librespot-container

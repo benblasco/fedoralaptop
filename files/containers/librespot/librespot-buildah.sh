37b71166e96821dfae979f44ee4d8d01eff54a47
Benjamin Blasco <you@example.com>
2022-04-14 23:33:08 +1000

    Updated cargo install command and speaker name

--- files/containers/librespot/librespot-buildah.sh
+++ files/containers/librespot/librespot-buildah.sh
@@ -5,10 +5,12 @@ CONTAINER=$(buildah from registry.fedoraproject.org/fedora-minimal:35)
 buildah run ${CONTAINER} microdnf install -y cargo alsa-lib-devel make gcc
 buildah run ${CONTAINER} microdnf clean all
 buildah run ${CONTAINER} rm -rf /var/cache/dnf
-buildah run ${CONTAINER} cargo install librespot
+#buildah run ${CONTAINER} cargo install librespot
+buildah run ${CONTAINER} cargo install librespot --no-default-features --features "alsa-backend"
 
 buildah config --entrypoint "/root/.cargo/bin/librespot" ${CONTAINER} 
-buildah config --cmd "--name=NUC LibreSpot Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
+#buildah config --cmd "--name=NUC LibreSpot Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
+buildah config --cmd "--name=Test LibreSpot Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
 
 buildah config --label maintainer="Benjamin Blasco" ${CONTAINER}
 buildah config --label description="Fedora-based container for running librespot, built on Rust" ${CONTAINER}

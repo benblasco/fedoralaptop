160d4b5f3509cdf516d7bbfad5fea6bd7c4193fa
Benjamin Blasco <you@example.com>
2022-04-16 21:23:05 +1000

    Updated for Fedora 35 Compiled binary works when copied out of the container and run as a regular user with the following command: ./librespot --name="Ben" --bitrate=320 --zeroconf-port=49999 --initial-volume=75 -v --device=default

--- files/containers/librespot/librespot-buildah.sh
+++ files/containers/librespot/librespot-buildah.sh
@@ -2,15 +2,14 @@
 
 CONTAINER=$(buildah from registry.fedoraproject.org/fedora-minimal:35)
 
-buildah run ${CONTAINER} microdnf install -y cargo alsa-lib-devel make gcc
+buildah run ${CONTAINER} microdnf install -y cargo make gcc alsa-lib-devel 
 buildah run ${CONTAINER} microdnf clean all
 buildah run ${CONTAINER} rm -rf /var/cache/dnf
-#buildah run ${CONTAINER} cargo install librespot
 buildah run ${CONTAINER} cargo install librespot --no-default-features --features "alsa-backend"
 
 buildah config --entrypoint "/root/.cargo/bin/librespot" ${CONTAINER} 
-#buildah config --cmd "--name=NUC LibreSpot Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
-buildah config --cmd "--name=Test LibreSpot Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
+#buildah config --cmd "--name=Buildah_LibreSpot_Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
+buildah config --cmd "--name=Buildah_LibreSpot_Speaker --bitrate=320 --zeroconf-port=49999 --backend=alsa --device=default --initial-volume=75 -v" ${CONTAINER}
 
 buildah config --label maintainer="Benjamin Blasco" ${CONTAINER}
 buildah config --label description="Fedora-based container for running librespot, built on Rust" ${CONTAINER}

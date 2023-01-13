51667b8d47e7b0c221e2a780d1ed9c1c89cef3d4
Benjamin Blasco <bblasco@redhat.com>
2023-01-13 12:21:28 +1100

    New files to try and use Network Dispatcher To deal with my darned autofs problem

---
+++ roles/workstation/files/autofs/disable_autofs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Put this script in /etc/NetworkManager/dispatcher.d/pre-down.d
+# Ensure the script is owned by root and executable
+if [ "$2" = "up" ]; then
+  if ["$CONNECTION_UUID" = "e40fc7a7-0055-475c-85d7-9f630c1a5cfe" ]; then
+    systemctl disable --now autofs.service
+  fi
+fi

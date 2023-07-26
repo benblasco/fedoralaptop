f7fbb92c526404b5e7327eecf42cc5e5ab4cd208
Benjamin Blasco <bblasco@redhat.com>
2023-07-27 00:38:51 +1000

    Updated condition to disable/enable autofs

--- roles/workstation/files/autofs/disable-autofs.sh
+++ roles/workstation/files/autofs/disable-autofs.sh
@@ -5,7 +5,8 @@ echo "*** The CONNECTION ID is $CONNECTION_ID, and the CONNECTION UUID is $CONNE
 if [ "$2" == "pre-down" ]; then
   echo "*** We have determined that the connection is in state $2"
   #if [ "$CONNECTION_UUID" = "2911db45-3eff-3b74-9c87-4c36e0290693" ]; then
-  if [ "$CONNECTION_ID" == "eraser215" ]; then
+  # if [ "$CONNECTION_ID" == "eraser215" ]; then
+  if [ "$CONNECTION_ID" == "eraser215" ] || [ "$CONNECTION_ID" == "Wired connection 1" ]; then
     echo "*** Disabling autofs"
     systemctl disable --now autofs.service
   fi
--- roles/workstation/files/autofs/enable-autofs.sh
+++ roles/workstation/files/autofs/enable-autofs.sh
@@ -5,7 +5,8 @@ echo "*** The CONNECTION ID is $CONNECTION_ID, and the CONNECTION UUID is $CONNE
 if [ "$2" == "pre-up" ]; then
   echo "*** We have determined that the connection is in state $2"
   #if [ "$CONNECTION_UUID" == "2911db45-3eff-3b74-9c87-4c36e0290693" ]; then
-  if [ "$CONNECTION_ID" = "eraser215" ]; then
+  #if [ "$CONNECTION_ID" = "eraser215" ]; then
+  if [ "$CONNECTION_ID" == "eraser215" ] || [ "$CONNECTION_ID" == "Wired connection 1" ]; then
     echo "*** Enabling autofs"
     systemctl enable --now autofs.service
   fi
